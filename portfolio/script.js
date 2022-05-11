67f611f8bcdbc31b09e66168ed5e925abde17fa7
TomisinAjayi <thomieajayi@gmail.com>
2022-05-11 19:37:00 +0100

    created contact and fillied in dead links

--- portfolio/script.js
+++ portfolio/script.js
@@ -14,3 +14,13 @@ for (var i = 0; i < list.length; i++) {
   this.className += " active";
   });
 }
+
+var projectList = document.getElementById("projectList");
+var projectLang = projectList.getElementsByClassName("list");
+for (var x = 0; x < projectLang.length; x++) {
+  projectLang[x].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active-project");
+  current[0].className = current[0].className.replace(" active-project", "");
+  this.className += " active-project";
+  });
+}
