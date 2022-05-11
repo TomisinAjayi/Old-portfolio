f7cc81d4a8ddd6ef3559fbafa64e999694b1a10f
TomisinAjayi <thomieajayi@gmail.com>
2022-05-11 23:39:34 +0100

    linked the contact form to a mail server

--- portfolio/script.js
+++ portfolio/script.js
@@ -24,3 +24,24 @@ for (var x = 0; x < projectLang.length; x++) {
   this.className += " active-project";
   });
 }
+
+// contact mail
+(function() {
+  // https://dashboard.emailjs.com/admin/account
+  emailjs.init('1kutXdqilnCQZV1ga');
+})();
+
+window.onload = function() {
+  document.getElementById('contact_form').addEventListener('submit', function(event) {
+      event.preventDefault();
+      // generate a five digit number for the contact_number variable
+      this.contact_number.value = Math.random() * 100000 | 0;
+      // these IDs from the previous steps
+      emailjs.sendForm('contact_service', 'contact_form', this)
+          .then(function() {
+              console.log('SUCCESS!');
+          }, function(error) {
+              console.log('FAILED...', error);
+          });
+  });
+}
